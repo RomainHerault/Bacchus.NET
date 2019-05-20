188b6bcdda7fc69f53461ac54342cd44c35b7fce
RomainHerault <heraultromain8@gmail.com>
2019-05-20 15:20:58 +0200

    Avancement du parser

--- Bacchus/Bacchus/Utils/Parser.cs
+++ Bacchus/Bacchus/Utils/Parser.cs
@@ -32,14 +32,19 @@ namespace Bacchus.Utils
             //Description; Ref; Marque; Famille; Sous - Famille; Prix H.T.
             string Description = Objects[0];
             string Ref = Objects[1];
-            string Brand = Objects[2];
+            Brand Brand = new Brand(Objects[2]);
             Category Category = new Category(Objects[3]);
             SubCategory SubCategory = new SubCategory(Objects[4], Category);
             int Price = 0 ;
+            Product Product = new Product(Description, Ref, Brand, SubCategory, Price, 0);
+
             Int32.TryParse(Objects[5],out Price);
 
+            //Add to the BD
             int BrandId = BrandDAO.AddBrand(Brand);
-           // int CategoryId = CategoryDAO.AddCategory();
+            int CategoryId = CategoryDAO.AddCategory(Category);
+            int SubCategoryId = SubCategoryDAO.AddSubCategory(SubCategory, CategoryId);
+            ProductDAO.AddProduct(Product, SubCategoryId, BrandId);
 
 
 
